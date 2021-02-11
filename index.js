12cd8c0e553b5f1dc8609150798ec54e0f49ed5c
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-02-10 21:32:38 -0300

    more changes

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 var productName = document.getElementById('name')
 var produto = document.getElementById('product')
 var produtos = document.getElementById('produtos')
+var quantidade = document.getElementById('quantidade')
 
 var nameprodutos = []
 
 function cadastrarProduto(){
 
-    if(productName.value === ''){
-        window.alert('Campo de Pesquisa Vazio!')
-    }
+    if(productName.value !== '' && quantidade.value !== ''){
+        nameprodutos.push({name:productName.value,quantidade:Number(quantidade.value)})
 
-    nameprodutos.push({name:productName.value})
-    
         var newDiv = document.createElement('div')
         produtos.appendChild(newDiv)
+        newDiv.id = 'produto'
 
         var newName = document.createElement('p')
-        var theName = document.createTextNode(productName.value)
+        var theName = document.createTextNode(`Produto: ${productName.value}`)
         newName.appendChild(theName)
         newDiv.appendChild(newName)
+        newName.id = 'product-name'
 
-    productName.value = ''
-    
+        var newQuantidade = document.createElement('strong')
+        var theQuantidade = document.createTextNode(`Quantidade: ${quantidade.value}`)
+        newQuantidade.appendChild(theQuantidade)
+        newDiv.appendChild(newQuantidade)
+        newQuantidade.id = 'quantidade-number'
+        
+        productName.value = ''
+        quantidade.value = ''
+
+        console.log(nameprodutos)
+    }
+else{
+    return window.alert('Campo de pesquisa vazio')    
+}
 }
 
 document.getElementById('add').onclick = cadastrarProduto
